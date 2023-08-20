22e2d5cf94f4b16f746dbc581a4ea3f4bdc855f4
Rok <seoerok@gmail.com>
2023-08-19 22:46:15 -1000

    .

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
-const generateButton = document.getElementById('generateButton');
-const gridSizeInput = document.getElementById('gridSizeInput');
-const gridContainer = document.querySelector('.grid-container');
+const grid = document.getElementById('grid');
+const sizeSlider = document.getElementById('sizeSlider');
+const sizeValue = document.getElementById('sizeValue');
+const gridItem = document.getElementById('grid-item');
+const colorSelect = document.getElementById('color');
+const clearBtn = document.getElementById('clear');
 
-function createGrid(n) {
-    gridContainer.innerHTML = ''; // Clear existing grid items
-    
-    gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
-    
-    for (let i = 0; i < n * n; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridContainer.appendChild(gridItem);
 
-        gridItem.addEventListener('mouseover', () => {
-            gridItem.style.backgroundColor = 'black';
+let color = 'black';
+function makeGrid(size) {
+    grid.style.setProperty('--grid-rows', size);
+    grid.style.setProperty('--grid-cols', size);
+    for (i = 0; i < (size * size); i++) {
+        let cell = document.createElement('div');
+        grid.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseover', ()=> {
+            cell.style.backgroundColor = color;
         });
-    }
+        
+    };
+};
+
+function clearGrid(){
+    let element = document.querySelectorAll('.grid-item');
+    element.forEach(item =>{
+        item.remove();
+    })
+};
+
+
+clearBtn.addEventListener('click', ()=>{
+    clearGrid();
+    makeGrid(sizeSlider.value);
+})
+
+colorSelect.addEventListener('input', ()=>{
+    color = colorSelect.value
+
+})
+
+sizeSlider.onchange = () =>{
+    sizeValue.innerHTML = `Size: ${sizeSlider.value} x ${sizeSlider.value}`;
+    clearGrid();
+    makeGrid(sizeSlider.value);
 }
 
-generateButton.addEventListener('click', () => {
-    const gridSize = parseInt(gridSizeInput.value);
-    if (!isNaN(gridSize)) {
-        createGrid(gridSize);
-    }
-});
-
+makeGrid(32);
 
